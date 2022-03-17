8a488b9a3e59ec04ba0eb3bb80c68a34ed5a3ed3
liuxili01 <liuxili@bestseller.com.cn>
2022-03-17 17:37:07 +0800

    gateway初识

---
+++ api-gateway/src/main/java/com/rookie/bigdata/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.rookie.bigdata;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname UserApplication
+ * @Description TODO
+ * @Author rookie
+ * @Date 2022/3/14 17:34
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+}
